ad80696651c17dc9e0c233c8e09baa698761572d
dougluz <dougs.2210@gmail.com>
2024-12-30 20:18:09 -0300

    added deck class

---
+++ lib/deck.rb
@@ -0,0 +1,25 @@
+require_relative "card"
+
+class Deck
+  SUITS = %w[Hearts Diamonds Clubs Spades]
+  RANKS = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace]
+
+  attr_reader :cards
+
+  def initialize
+    @cards = build_deck
+    shuffle!
+  end
+
+  def build_deck
+    SUITS.product(RANKS).map { |suit, rank| Card.new(rank, suit) }
+  end
+
+  def suffle!
+    cards.suffle!
+  end
+
+  def deal
+    cards.pop
+  end
+end
